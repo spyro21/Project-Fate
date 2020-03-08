86e37610d20cff17a4f1f6c90de515a9d2ebdf40
spyro21 <38793743+spyro21@users.noreply.github.com>
2020-03-08 15:31:40 -0500

    wut

---
+++ Project Fate/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed;
+    private Vector2 movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+}
